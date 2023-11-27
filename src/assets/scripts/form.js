88c19aa5eee6ff54142379558ef54d85cddf1813
Roser Fuster Bertomeu <rfusterb@uoc.edu>
2023-11-27 13:23:41 +0100

    form.js jquery

--- src/assets/scripts/form.js
+++ src/assets/scripts/form.js
@@ -1,20 +1,21 @@
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => {
-      $("contacte__success").fadeIn();
-      setTimeout($("contacte__success").fadeOut(), 2000);
-    })
-    .catch((error) => {
-      $("contacte__danger").fadeIn();
-      setTimeout($("contacte__danger").fadeOut(), 2000);
+$(document).ready(function () {
+  $(".alert").hide();
+  $(".formulari__contacte").submit(function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: "/",
+      type: "POST",
+      data: new FormData(this),
+      processData: false,
+      contentType: false,
+      success: function () {
+        $(".contacte__success").fadeIn();
+        setTimeout($(".contacte__success").fadeOut(), 2000);
+      },
+      error: function (error) {
+        $(".contacte__danger").fadeIn();
+        setTimeout($(".contacte__danger").fadeOut(), 2000);
+      }
     });
-};
+  });
+});
