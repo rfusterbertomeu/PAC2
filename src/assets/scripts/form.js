e29837dc35d223a6b955ec7c97139e8671d1626e
Roser Fuster <rfusterb@uoc.edu>
2023-11-27 20:18:58 +0100

    correccions formulari carrega DOM

--- src/assets/scripts/form.js
+++ src/assets/scripts/form.js
@@ -1,27 +1,32 @@
 import $ from "jquery";
-$(".alert").hide();
-var success = $(".contacte__success");
-var fail = $(".contacte__fail");
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-  
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => { success.fadeIn();
-      setTimeout(function() {
-        success.fadeOut();
-      }, 2000);})
-    .catch((error) => { success.fadeIn();
-      setTimeout(function() {
-        fail.fadeOut();
-      }, 2000);});
-};
 
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
+$(function() {
+    
+  $(".alert").hide();
+  var success = $(".contacte__success");
+  var fail = $(".contacte__fail");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => { success.fadeIn();
+        setTimeout(function() {
+          success.fadeOut();
+        }, 2000);})
+      .catch((error) => { success.fadeIn();
+        setTimeout(function() {
+          fail.fadeOut();
+        }, 2000);});
+  };
+
+  document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
+});
