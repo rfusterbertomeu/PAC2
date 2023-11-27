9dd8eb74fdf67284c82b3e0fccb00919ee91dcb1
Roser Fuster <rfusterb@uoc.edu>
2023-11-27 19:51:37 +0100

    correccions ajax

--- src/assets/scripts/form.js
+++ src/assets/scripts/form.js
@@ -2,28 +2,29 @@ import $ from "jquery";
 
 $(document).ready(function () {
   $(".alert").hide();
-  $(".formulari__contacte").submit(function (event) {
+  $("form[name='formulari__contacte']").submit(function (event) {
     event.preventDefault();
+
     var success = $(".contacte__success");
     var fail = $(".contacte__fail");
+    console.log(event);
+    console.log(success);
     $.ajax({
       url: "/",
       type: "POST",
-      data: new FormData(this),
-      success: function () {
+      data: new FormData(event.target)
+    }).done(function () {
         console.log("success");
         success.fadeIn();
         setTimeout(function() {
           success.fadeOut();
         }, 2000);
-      },
-      error: function (error) {
+      }).fail(function (error) {
         console.log(error);
         fail.fadeIn();
         setTimeout(function() {
           fail.fadeOut();
         }, 2000);
-      }
-    });
+      });
   });
 });
